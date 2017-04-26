7d928ff5d7effc4a7e22d96c6487e7ab55ec9ca1
lyzmusic <635716260@qq.com>
2017-04-26 16:23:38 -0400

    -debug session init

--- A3C/A3C-gym-train.py
+++ A3C/A3C-gym-train.py
@@ -21,7 +21,7 @@ from tensorpack.predict.common import PredictConfig, get_predict_func
 from tensorpack import *
 from tensorpack.models.model_desc import ModelDesc, InputVar
 from tensorpack.train.config import TrainConfig
-from tensorpack.tfutils.sessinit import SessionInit
+from tensorpack.tfutils.sessinit import SessionInit, JustCurrentSession
 from tensorpack.tfutils.common import *
 from tensorpack.tfutils.tower import get_current_tower_context
 from tensorpack.callbacks.group import Callbacks
@@ -207,7 +207,7 @@ class MySimulatorMaster(SimulatorMaster, Callback):
             sessinit = SessionInit()
             sessinit.init(self.sess)
             cfg = PredictConfig(
-                    session_init=sessinit,
+                    session_init=JustCurrentSession(),
                     model=Model(),
                     input_var_names=['state'],
                     output_var_names=[FEATURE])
@@ -220,7 +220,7 @@ class MySimulatorMaster(SimulatorMaster, Callback):
                 sessinit = SessionInit()
                 sessinit.init(self.sess)
                 cfg = PredictConfig(
-                    session_init=sessinit,
+                    session_init=JustCurrentSession(),
                     model = Model(),
                     input_var_names=['state'],
                     output_var_names=[FEATURE])
